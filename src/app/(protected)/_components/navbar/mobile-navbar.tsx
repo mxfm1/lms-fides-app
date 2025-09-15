af208956fb746afb3bf799d273d1ea39078a4811
mxfm1 <1f.andresM@gmail.com>
2025-09-14 22:43:44 -0300

    fix mobile sidebar behavior

--- src/app/(protected)/_components/navbar/mobile-navbar.tsx
+++ src/app/(protected)/_components/navbar/mobile-navbar.tsx
@@ -7,6 +7,7 @@ import { NotifButton } from "../notifications/notif-button"
 import { DropdownUserButton } from "../dropdown-user-content"
 import { useRouter } from "next/navigation"
 import { LogoutButton } from "@/components/buttons/logout-button"
+import { useState } from "react"
 
 export  const MobileMenu = ({
     isLoggedIn,
@@ -16,9 +17,14 @@ export  const MobileMenu = ({
     userData: UserClientData
   }) => {
     const { openModal } = UseAuthModal()
+    const [open,setIsOpen] = useState(false)
+
+    const onMobileLogoutButton = () => {
+        setIsOpen(false)
+    }
   
     return (
-      <Sheet>
+      <Sheet onOpenChange={setIsOpen} open={open}>
         <SheetTrigger asChild>
           <Button variant="ghost" size="lg" className="hover:bg-secondary hover:cursor-pointer" >
             <Menu className="w-5 h-5"/>
@@ -26,7 +32,7 @@ export  const MobileMenu = ({
         </SheetTrigger>
         <SheetContent side="left" className="w-[75%] sm:w-[300px] p-4">
           <SheetHeader>
-            <SheetTitle className="text-lg font-bold">FIdes LMS</SheetTitle>
+            <SheetTitle className="text-lg font-bold">Fides LMS</SheetTitle>
           </SheetHeader>
   
           <div className="flex flex-col gap-4 mt-6 h-full">
@@ -36,7 +42,7 @@ export  const MobileMenu = ({
                 <MobileSidebarItem label="Inicio" href="/home" icon={Home}/>
 
                 <div className="mt-auto mb-4">
-                    <LogoutButton className="bg-red-500"/>
+                    <LogoutButton className="bg-red-500" onLoggedOut={onMobileLogoutButton}/>
                 </div>
                 {/* <NotifButton /> */}
                 {/* <DropdownUserButton data={userData} /> */}
@@ -52,14 +58,20 @@ export  const MobileMenu = ({
                 <div className="mt-auto flex gap-2">
                   <Button
                     className="flex-1"
-                    onClick={() => openModal()}
+                    onClick={() => {
+                        openModal()
+                        setIsOpen(false)
+                    }}
                     variant="outline"
                   >
                     Iniciar sesión
                   </Button>
                   <Button
                     className="flex-1"
-                    onClick={() => openModal("register")}
+                    onClick={() => {
+                        openModal("register")
+                        setIsOpen(false)
+                    }}
                     variant="default"
                   >
                     Registrarse
