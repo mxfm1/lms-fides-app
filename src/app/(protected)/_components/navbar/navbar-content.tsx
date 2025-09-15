03062b2c4320cf9f8a00044030ecab6b09ba080e
mxfm1 <1f.andresM@gmail.com>
2025-09-15 08:46:22 -0300

    add console log de la ruta de prod

--- src/app/(protected)/_components/navbar/navbar-content.tsx
+++ src/app/(protected)/_components/navbar/navbar-content.tsx
@@ -19,6 +19,7 @@ export default function NavbarContent({
 
     const {openModal} = UseAuthModal()
     console.log("PROPS DATA",data,isLoggedIn)
+    console.log("production url env", process.env.PUBLIC_APP_URL)
     return (
     <nav className="border-b border-border/50 bg-background/80 backdrop-blur-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
